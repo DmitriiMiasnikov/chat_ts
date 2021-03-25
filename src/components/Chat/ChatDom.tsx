9f42f02c80b7a0479525d4ce70e2171f2719e7b2
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-03-25 15:58:19 +0300

    styled chat

--- src/components/Chat/ChatDom.tsx
+++ src/components/Chat/ChatDom.tsx
@@ -1,34 +1,67 @@
-import React from 'react';
-import styles from './Chat.module.scss';
-import Message from './Message/Message';
-import CreateMessage from './CreateMessage/CreateMessage';
-import { Fetching } from './../Fetching';
+import React from "react";
+import Message from "./Message/Message";
+import CreateMessage from "./CreateMessage/CreateMessage";
+import { Fetching } from "./../Fetching";
+import styled from "styled-components";
+
+const StyledChatWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: inherit;
+`;
+const StyledMessages = styled.div`
+  padding: 10px;
+  margin: 5px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    width: 15px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: #1d1d1d;
+  }
+`;
 
 type Props = {
-  messages: any,
-  currentUser: { email: string, userName: string, _id: string },
-  refEndList: any,
-  refMessages: any,
-  fetching: boolean,
-  setFetching: (fetching: boolean) => void,
-  chatId: string,
-  scrollToBottom: () => void
-}
+  messages: any;
+  currentUser: { email: string; userName: string; _id: string };
+  refEndList: any;
+  refMessages: any;
+  fetching: boolean;
+  setFetching: (fetching: boolean) => void;
+  chatId: string;
+  scrollToBottom: () => void;
+};
 
 export const ChatDom = (props: Props) => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.messages} ref={props.refMessages}>
-        {props.fetching && <Fetching blockWidth={'100%'} blockHeight={'40px'} imageSize={'35px'} />}
+    <StyledChatWrapper>
+      <StyledMessages ref={props.refMessages}>
+        {props.fetching && (
+          <Fetching
+            blockWidth={"100%"}
+            blockHeight={"40px"}
+            imageSize={"35px"}
+          />
+        )}
         {props.messages.map((el: any, i: number) => {
-          return (
-            <Message message={el} key={i} />
-          )
+          return <Message message={el} key={i} />;
         })}
         <div ref={props.refEndList} />
-      </div>
-      {props.currentUser && <CreateMessage setFetching={props.setFetching} chatId={props.chatId}
-        scrollToBottom={props.scrollToBottom} />}
-    </div>
-  )
-}
+      </StyledMessages>
+      {props.currentUser && (
+        <CreateMessage
+          setFetching={props.setFetching}
+          chatId={props.chatId}
+          scrollToBottom={props.scrollToBottom}
+        />
+      )}
+    </StyledChatWrapper>
+  );
+};
